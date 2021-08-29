90ac421520d3dda5db3fdc40bd0e7f3d6cef6ac8
Vitor Salles <vitorsalles.santos@hotmail.com>
2021-08-29 11:31:35 -0300

    Comecando a implementar o metodo que imprime o tabuleiro

--- Projeto_Xadrez/Tabuleiro/Tabuleiro.cs
+++ Projeto_Xadrez/Tabuleiro/Tabuleiro.cs
@@ -4,13 +4,18 @@
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        public Peca[,] Pecas;
+        private Peca[,] Pecas;
 
         public Tabuleiro(int linhas, int colunas)
         {
             Linhas = linhas;
             Colunas = colunas;
             Pecas = new Peca[Linhas, Colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
         }
     }
 }
