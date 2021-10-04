2cf82ec84c23df1a3f6a94a46061b5c4f2dbef8d
Vitor Salles <vitorsalles.santos@hotmail.com>
2021-10-04 20:16:14 -0300

    Jogada especial Promocao

--- Projeto_Xadrez/xadrez/PartidaDeXadrez.cs
+++ Projeto_Xadrez/xadrez/PartidaDeXadrez.cs
@@ -136,6 +136,22 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = Tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -154,7 +170,6 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = Tab.peca(destino);
             // #jogadaespecial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
             {
